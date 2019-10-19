6312af905b5dfcb9534b25b8cf90eff9c5c46e44
CheezItMan <chris@adadevelopersacademy.org>
2019-10-19 09:58:31 -0700

    updated config settings for Ada repos

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const GITHUB_BASE_URL = 'https://api.github.com/';
 
 // TODO: these values should be updated to point at real Ada repos/orgs/team IDs
-export const GITHUB_ORGS = ['ada-code-review', 'Ada-C12'];
-export const GITHUB_ROLE_ORGANIZATION = `ada-code-review`;
-export const VOLUNTEER_TEAM_ID = `3364060`;
-export const INSTRUCTOR_TEAM_ID = `3364061`;
+export const GITHUB_ORGS = ['Ada-Review-Test', 'Ada-C12'];
+export const GITHUB_ROLE_ORGANIZATION = `Ada-Review-Test`;
+export const VOLUNTEER_TEAM_ID = `3364060`;  // Added from Ada-Review-Test
+export const INSTRUCTOR_TEAM_ID = `3466669`; // Added from Ada-Review-Test
